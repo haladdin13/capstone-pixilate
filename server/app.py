37b74f28823e05cce2f269ac19e28d20f0fa1b23
Hussein Aladdin <haladdin13@outlook.com>
2024-03-15 18:15:18 -0400

    implemented palette, color, and color_association models and route functionality on backend

--- server/app.py
+++ server/app.py
@@ -70,6 +70,13 @@ class Logout(Resource):
 api.add_resource(Logout, '/logout', endpoint='logout')
 
 
+allowed_endpoints = ['signup', 'login', 'check_session']
+@app.before_request
+def check_if_logged_in():
+    if not session.get('user_id') and request.endpoint not in allowed_endpoints:
+        return {'error': 'Unauthorized'}, 401
+
+
 # Views go here!
 
 @app.route('/')
@@ -77,6 +84,192 @@ def index():
     return '<h1>Project Server</h1>'
 
 
+
+
+#Get all palettes
+    
+class PaletteGallery(Resource):
+    def get(self):
+
+        palette_list = [palette.to_dict() for palette in Palette.query.all()]
+        
+        response = make_response(palette_list, 200)
+
+        return response
+    
+
+    def post(self):
+
+        json_data = request.get_json()
+
+        try:
+            new_palette = Palette(
+                title = json_data['title'],
+                description = json_data['description'],
+                tags = json_data['tags'],
+                likes = json_data['likes'],
+                public = json_data['public'],
+                user_id = json_data['user_id']
+            )
+
+            db.session.add(new_palette)
+            db.session.commit()
+
+            response = make_response(new_palette.to_dict(), 201)
+
+            return response
+        except Exception as e:
+            return make_response({'error': str(e) }, 422)
+    
+api.add_resource(PaletteGallery, '/palettes', endpoint='palettes')
+
+#Get a palette by ID
+
+class PaletteID(Resource):
+    def get(self, id):
+
+        palette = Palette.query.filter(Palette.id == id).first()
+
+        if palette:
+            response = make_response(palette.to_dict(), 200)
+        else:
+            response = make_response({'error': 'Palette not found'}, 404)
+
+        return response
+    
+    def patch(self, id):
+        json_data = request.get_json()
+        palette = Palette.query.filter(Palette.id == id).first()
+
+        if palette:
+            palette.title = json_data['title']
+            palette.description = json_data['description']
+            palette.tags = json_data['tags']
+            palette.likes = json_data['likes']
+            palette.public = json_data['public']
+            palette.user_id = json_data['user_id']
+
+            db.session.commit()
+
+            response = make_response(palette.to_dict(), 200)
+        else:
+            response = make_response({'error': 'Palette not found'}, 404)
+
+        return response
+    
+    def delete(self, id):
+        palette = Palette.query.filter(Palette.id == id).first()
+
+        if palette:
+            db.session.delete(palette)
+            db.session.commit()
+
+            response = make_response(palette.to_dict(), 200)
+        else:
+            response = make_response({'error': 'Palette not found'}, 404)
+
+        return response
+    
+api.add_resource(PaletteID, '/palettes/<int:id>', endpoint='palettes_id')
+
+
+#Get Palette Galley by User ID
+
+
+class PaletteGalleryByUserID(Resource):
+    def get(self, id):
+        palette_list = [palette.to_dict() for palette in Palette.query.filter(Palette.user_id == id).all()]
+        
+        response = make_response(palette_list, 200)
+
+        return response
+    
+
+
+api.add_resource(PaletteGalleryByUserID, '/palettes/user/<int:id>', endpoint='palettes_user_id')
+
+#Get all colors
+
+class ColorResource(Resource):
+    def get(self):
+
+        color_list = [color.to_dict() for color in Color.query.all()]
+        
+        response = make_response(color_list, 200)
+
+        return response
+    
+
+    def post(self):
+
+        #check if color exists, if exists, increment usage frequency by 1
+        json_data = request.get_json()
+        hex_code = json_data.get('hex_code')
+        color = Color.query.filter_by(hex_code=hex_code).first()
+
+        if color:
+            color.usage_frequency += 1
+            db.session.commit()
+
+            response = make_response(color.to_dict(), 200)
+        else:
+            new_color = Color(
+                hex_code = hex_code,
+                usage_frequency = 1
+            )
+
+            db.session.add(new_color)
+            db.session.commit()
+
+            response = make_response(new_color.to_dict(), 201)
+
+        return response
+    
+api.add_resource(ColorResource, '/colors', endpoint='colors')
+
+
+#Get/Post Color Association
+
+class ColorAssociationRes(Resource):
+    def get(self):
+        color_association_list = [color_association.to_dict() for color_association in ColorAssociation.query.all()]
+        return make_response(color_association_list, 200)
+
+    def post(self):
+        json_data = request.get_json()
+        palette_id = json_data.get('palette_id')
+        color_ids = json_data.get('color_id')
+        color_id_list = [color_id for color_id in color_ids]
+
+        if not isinstance(color_id_list, list):
+            return make_response({'error': 'color_ids must be a list'}, 400)
+
+        palette = Palette.query.get(palette_id)
+        if not palette:
+            return make_response({'error': 'Palette not found'}, 404)
+
+        for color_id in color_ids:
+            color = Color.query.get(color_id)
+            if not color:
+
+                return make_response({'error': f'Color with id {color_id} not found'}, 404)
+
+            existing_association = ColorAssociation.query.filter_by(palette_id=palette_id, color_id=color_id).first()
+            if existing_association:
+                continue
+
+            combined_scores = (1 + palette.likes / 100) * color.usage_frequency
+            color_association = ColorAssociation(palette_id=palette_id, color_id=color_id, combined_scores=combined_scores)
+            db.session.add(color_association)
+            db.session.commit()
+            return make_response(color_association.to_dict(), 201)
+        # else:
+        #     return make_response({'error': 'No colors found'}, 404)
+        
+
+    
+api.add_resource(ColorAssociationRes, '/color_associations', endpoint='color_associations')
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
--- server/models.py
+++ server/models.py
@@ -16,6 +16,16 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String)
     user_avatar = db.Column(db.String)
+    
+    #RELATIONSHIPS
+
+    palettes = db.relationship('Palette', back_populates='user')
+
+    #SERIALIZATION RULES
+
+    serialize_rules = ('-_password_hash', '-palettes.user')
+
+    #PROPERTIES
 
     @hybrid_property
     def password_hash(self):
@@ -29,8 +39,12 @@ class User(db.Model, SerializerMixin):
 
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+
+    def __repr__(self):
+        return f'User: {self.username}, Email: {self.email}, Avatar: {self.user_avatar}'
     
-    serialize_rules = ('-_password_hash',)
+    #VALIDATION
 
     @validates('username')
     def validate_username(self, key, value):
@@ -55,3 +69,99 @@ class User(db.Model, SerializerMixin):
         if not isinstance(value, str) and len(value) == 0:
             raise ValueError('Password must be a non-empty string.')
         return value
+    
+
+class Palette(db.Model, SerializerMixin):
+    __tablename__ = 'palettes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    tags = db.Column(db.String)
+    likes = db.Column(db.Integer)
+    public = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #RELATIONSHIPS
+
+    user = db.relationship('User', back_populates='palettes')
+    color_associations = db.relationship('ColorAssociation', back_populates='palette')
+
+    #SERIALIZATION RULES
+
+    serialize_rules = ('-user.palettes', '-color_associations.palette', '-color_associations.color.palettes')
+
+    def __repr__(self):
+        return f'Palette: {self.title}, Description: {self.description}, Tags: {self.tags}, Likes: {self.likes}, Public: {self.public}'
+
+    #VALIDATION
+
+    @validates('title')
+    def validate_title(self, key, value):
+        if not isinstance(value, str) and len(value) == 0:
+            raise ValueError('Title must be a non-empty string.')
+        return value
+    
+    @validates('description')
+    def validate_description(self, key, value):
+        if not isinstance(value, str) and len(value) == 0:
+            raise ValueError('Description must be a non-empty string.')
+        return value
+    
+    @validates('tags')
+    def validate_tags(self, key, value):
+        if not isinstance(value, str) and len(value) < 3:
+            raise ValueError ('Tags must be a string longer than 2 characters.')
+        return value
+
+
+class Color(db.Model, SerializerMixin):
+    __tablename__ = 'colors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    hex_code = db.Column(db.String)
+    usage_frequency = db.Column(db.Integer)
+
+    #RELATIONSHIPS
+
+    color_associations = db.relationship('ColorAssociation', back_populates='color')
+
+    #SERIALIZATION RULES
+
+    serialize_rules = ('-color_associations.color', '-color_associations.palette')
+
+
+
+    def __repr__(self):
+        return f'Color: {self.hex_code}, Usage Frequency: {self.usage_frequency}'
+
+    #VALIDATION
+
+    #Check if hex string is valid
+    @validates('hex_code')
+    def validate_hex_code(self, key, value):
+        if not isinstance(value, str):
+            raise ValueError('Hex code must be a string.')
+        elif not (len(value) == 4 or len(value) == 5 or len(value) == 7 or len(value) == 9) and value.startswith('#'):
+            raise ValueError('Hex code must be #RGB, #RGBA, #RRGGBB, or #RRGGBBAA.')
+        return value
+    
+
+class ColorAssociation(db.Model, SerializerMixin):
+    __tablename__ = 'color_associations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    palette_id = db.Column(db.Integer, db.ForeignKey('palettes.id'))
+    color_id = db.Column(db.Integer, db.ForeignKey('colors.id'))
+    combined_scores = db.Column(db.Float)
+
+    #RELATIONSHIPS
+
+    palette = db.relationship('Palette', back_populates='color_associations')
+    color = db.relationship('Color', back_populates='color_associations')
+
+    #SERIALIZATION RULES
+
+    serialize_rules = ('-palette.color_associations', '-color.color_associations')
